5f55df9be52d28eb7f5fc151156daa4ff2788f23
Jessie Woods <jessieallyn@gmail.com>
2021-10-17 18:07:26 -0500

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { OrderDashboardComponent } from './order-dashboard/order-dashboard.component';
+import { FirstFiveOrdersComponent } from './first-five-orders/first-five-orders.component';
+import { AllOtherOrdersComponent } from './all-other-orders/all-other-orders.component';
+import { LotteryWinningOrderComponent } from './lottery-winning-order/lottery-winning-order.component';
+import { SuperLotteryComponent } from './super-lottery/super-lottery.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrderDashboardComponent,
+    FirstFiveOrdersComponent,
+    AllOtherOrdersComponent,
+    LotteryWinningOrderComponent,
+    SuperLotteryComponent
   ],
   imports: [
     BrowserModule
