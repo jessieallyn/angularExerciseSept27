506e9e63eb8f54ebc3fc78232255326b0a1a4b4f
Jessie Woods <jessieallyn@gmail.com>
2021-10-17 18:06:34 -0500

    Create all-other-orders.component.ts

---
+++ src/app/all-other-orders/all-other-orders.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-all-other-orders',
+  templateUrl: './all-other-orders.component.html',
+  styleUrls: ['./all-other-orders.component.css']
+})
+export class AllOtherOrdersComponent implements OnInit {
+  @Input() orders: number[];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
